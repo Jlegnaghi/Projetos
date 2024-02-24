342efa4cb24385772ec353667f35b590b3c5487a
KoP | Legnaghi <jeffersonlegnaghi07@gmail.com>
2024-02-24 11:37:10 -0300

    mudando o banco

--- 0/good wine/conexao.php
+++ 0/good wine/conexao.php
@@ -1,14 +1,14 @@
 <?php
 // Parâmetros de conexão com o banco de dados
-/*$servername = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$database = "good_wines";*/
+$database = "good_wines";
 
-$servername = "localhost"; // Endereço do servidor do banco de dados
+/*$servername = "localhost"; // Endereço do servidor do banco de dados
 $username = "jeffersonlegnagh"; // Nome de usuário do banco de dados
 $password = ";Rzkz^n}sx}E"; // Senha do banco de dados
-$database = "jefferso_good_wine"; // Nome do banco de dados
+$database = "jefferso_good_wine"; // Nome do banco de dados*/
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $database);
