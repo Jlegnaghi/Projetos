ae00a59bdc713ff5d48514e28f2c0d8c7bcf5c99
KoP | Legnaghi <jeffersonlegnaghi07@gmail.com>
2024-02-24 11:19:52 -0300

    Merge branch 'main' of github.com:Jlegnaghi/Projetos

--- 2/contato.php
+++
@@ -1,8 +0,0 @@
-<?php include 'header.php'; ?>
-        <section>
-            <h2>Entre em Contato</h2>
-            <p>Se você tiver alguma dúvida ou precisar de assistência, entre em contato conosco:</p>
-            <p>Email: contato@goodwines.com</p>
-            <p>Telefone: (11) 1234-5678</p>
-        </section>
-<?php include 'footer.php'; ?>
--- 2/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer>
-        <p>&copy; 2024 Good Wines. Todos os direitos reservados.</p>
-    </footer>
-</body>
-</html>
